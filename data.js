52f8d715a3ae34a19442e78eb64314cfc7b7aee7
Felix Fassbinder <felix.fassbinder@gmx.de>
2023-12-11 10:44:17 +0100

    add questions and answers and define ids

---
+++ data.js
@@ -0,0 +1,178 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Indiana Jones",
+        "answer_2": "Princess Lea",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Jack Sparrow",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie heißt das größte Tier?",
+        "answer_1": "Elefant",
+        "answer_2": "Giraffe",
+        "answer_3": "Strauß",
+        "answer_4": "Blauwal",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie heißt der höchste Berg der Welt?",
+        "answer_1": "Zugspitze",
+        "answer_2": "Mount Everest",
+        "answer_3": "Mount Blanc",
+        "answer_4": "Annapurna",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie viele Flügel hat ein Schmetterling?",
+        "answer_1": "4",
+        "answer_2": "2",
+        "answer_3": "6",
+        "answer_4": "8",
+        "right_answer": 1
+    },
+    {
+        "question": "Auf welchem Kontinent liegt Deutschland?",
+        "answer_1": "Asien",
+        "answer_2": "Afrika",
+        "answer_3": "Nord Amerika",
+        "answer_4": "Europa",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie viele Spieler hat eine Fußballmannschaft?",
+        "answer_1": "9",
+        "answer_2": "10",
+        "answer_3": "11",
+        "answer_4": "12",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie heißt die Hauptstadt von Deutschland",
+        "answer_1": "Berlin",
+        "answer_2": "Hamburg",
+        "answer_3": "Bonn",
+        "answer_4": "Frankfurt",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche Form hat die Narbe auf Harry Potters Stirn?",
+        "answer_1": "Dreieick",
+        "answer_2": "Kreis",
+        "answer_3": "Kreuz",
+        "answer_4": "Blitz",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie nennt man einenj jungen Hund?",
+        "answer_1": "Fohlen",
+        "answer_2": "Rüde",
+        "answer_3": "Welpen",
+        "answer_4": "Ferkel",
+        "right_answer": 3
+    },
+    {
+        "question": "Welche Sprache sprechen Engländer?",
+        "answer_1": "Englisch",
+        "answer_2": "Deutsch",
+        "answer_3": "Französisch",
+        "answer_4": "Italienisch",
+        "right_answer": 1
+    },
+    {
+        "question": "An welchem Datum wird Heiligabend gefeiert?",
+        "answer_1": "23. Dezember",
+        "answer_2": "24. Dezember",
+        "answer_3": "25. Dezember",
+        "answer_4": "26. Dezember",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie viele Stacheln hat ein Igel?",
+        "answer_1": "50",
+        "answer_2": "500",
+        "answer_3": "1000",
+        "answer_4": "5000",
+        "right_answer": 4
+    },
+    {
+        "question": "Welches ist kein Tier?",
+        "answer_1": "Wollschwein",
+        "answer_2": "Lemur",
+        "answer_3": "Katamaran",
+        "answer_4": "Nacktmuhl",
+        "right_answer": 3
+    },
+    {
+        "question": "Welches ist das härteste Material auf der Erde?",
+        "answer_1": "Stahl",
+        "answer_2": "Diamand",
+        "answer_3": "Gold",
+        "answer_4": "Holz",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches ist die am häufigsten gesprochenne Sprache auf der Welt?",
+        "answer_1": "Mandarin Chinesisch",
+        "answer_2": "Japanisch",
+        "answer_3": "Deutsch",
+        "answer_4": "Spanisch",
+        "right_answer": 1
+    },
+    {
+        "question": "Wer ist der Gründer von Microsoft?",
+        "answer_1": "Abraham Lincon",
+        "answer_2": "Angela Merkel",
+        "answer_3": "Steve Jobs",
+        "answer_4": "Bill Gates",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie viele Herzen hat ein Wurm?",
+        "answer_1": "1",
+        "answer_2": "3",
+        "answer_3": "5",
+        "answer_4": "7",
+        "right_answer": 3
+    },
+    {
+        "question": "Welcher Planet in der Milchstraße ist am heißesten?",
+        "answer_1": "Venus",
+        "answer_2": "Erde",
+        "answer_3": "Saturn",
+        "answer_4": "Mars",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie viele Planeten gibt es in unserem Sonnensystem?",
+        "answer_1": "6",
+        "answer_2": "8",
+        "answer_3": "10",
+        "answer_4": "12",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches ist der kälteste Ort der Erde?",
+        "answer_1": "Alaska",
+        "answer_2": "Sibieren",
+        "answer_3": "Nordpol",
+        "answer_4": "Antarktis",
+        "right_answer": 4
+    },
+    {
+        "question": "Was stand auf dem Speiseplan der Jäger und Sammler in der Steinzeit?",
+        "answer_1": "Milch",
+        "answer_2": "Getreide",
+        "answer_3": "Eier",
+        "answer_4": "Nutella",
+        "right_answer": 3
+    },
+    {
+        "question": "Welches dieser Tiere gibt es nicht?",
+        "answer_1": "Schee-Eule",
+        "answer_2": "Polar-Igel",
+        "answer_3": "Eisvogel",
+        "answer_4": "Wüsten-fuchs",
+        "right_answer": 2
+    }
+];
