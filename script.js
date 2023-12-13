b08abf947a892d7c0c180d9eb458363e2a054d4f
Felix Fassbinder <felix.fassbinder@gmx.de>
2023-12-13 11:49:51 +0100

    add function to calculate number + to go to next question

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ function init() {
     showQuestion();
 }
 
-function showQuestion () {
- 
-    let question = questions[currentQuestion];
+function showQuestion() {
+    if (currentQuestion >= questions.length) {
+        document.getElementById('end_screen').style = '';
+        document.getElementById('question_body').style ="display: none"
+    }
+    else {
+    let question = questions[currentQuestion];  
+
+    showQuestionNumber();
     document.getElementById('questiontext').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
   
 }
 
@@ -27,5 +34,28 @@ function answer(selection) {
         document.getElementById(selection).parentNode.classList.add('bg-danger')
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
+    document.getElementById('next-button').disabled = false;
+}
 
+function nextQuestion() {
+    currentQuestion ++;
+    document.getElementById('next-button').disabled = true;
+    removeColor();
+    showQuestion();
+} 
+
+function removeColor() {
+ 
+document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function showQuestionNumber() {
+    document.getElementById('currect-question').innerHTML = currentQuestion + 1;
 }
